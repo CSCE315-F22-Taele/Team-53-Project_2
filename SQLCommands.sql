4cf31bb96a4cac2de47e92ed8f8be91b3ed7031e
Annie Ren <anniexren@gmail.com>
2022-10-03 15:43:26 -0500

    order inventory by expiration

--- SQLCommands.sql
+++ SQLCommands.sql
@@ -20,3 +20,19 @@ SELECT
     amount * cost AS total cost
 FROM 
     inventory;
+
+
+/* min, max, avg salaries */
+SELECT 
+    Max(salary), 
+    Min(salary), 
+    AVG(salary) 
+FROM employee;
+
+
+/* order inventory by expiration */
+SELECT * FROM inventory ORDER BY expirationdate;
+
+
+/* group inventory by vendor */
+
