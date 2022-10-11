d016948b42b2c7efcfe2d203e60069e2b3344ac4
Annie Ren <anniexren@gmail.com>
2022-10-11 15:00:18 -0500

    payment option buttons

--- gui/checkoutGUI.java
+++ gui/checkoutGUI.java
@@ -26,14 +26,25 @@ public class checkoutGUI{
     public checkoutGUI(){
         JButton cashButton = new JButton("CASH");
         cashButton.setBounds(130,100,100, 40);
+
+        JButton cardButton = new JButton("CREDIT / DEBIT");
+        cashButton.setBounds(130,200,100, 40);
         
-                ////////// Menu-items Area //////////
+        JButton ddButton = new JButton("DINING DOLLARS");
+        cashButton.setBounds(130,300,100, 40);
+
+        JButton mealswipeButton = new JButton("MEAL SWIPES");
+        cashButton.setBounds(130,400,100, 40);
+        ////////// Menu-items Area //////////
         JPanel itemsPanel = new JPanel();
         itemsPanel.setBackground(Color.yellow);
         itemsPanel.setBounds((int) (width * 0.06), (int) (height * 0.09), (int) (width * 0.6), (int) (height * 0.7));
         itemsPanel.setLayout(new GridLayout(5, 2, 10, 10)); 
 
         itemsPanel.add(cashButton);
+        itemsPanel.add(cardButton);
+        itemsPanel.add(ddButton);
+        itemsPanel.add(mealswipeButton);
 
         f.add(itemsPanel);
 
