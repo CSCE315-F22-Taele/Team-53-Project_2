f4401b3ce22578405ee426a8090edc7368068edf
Annie Ren <anniexren@gmail.com>
2022-10-11 14:53:06 -0500

    setup

---
+++ gui/checkoutGUI.java
@@ -0,0 +1,47 @@
+package gui;
+
+import java.awt.*;
+import javax.swing.*;
+import javax.swing.text.AbstractDocument.LeafElement;
+import java.sql.*;
+import java.sql.DriverManager;
+import java.awt.event.*;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;  
+import java.time.LocalDateTime;  
+import javax.swing.JOptionPane;
+import java.util.Calendar;
+import java.util.Date;
+
+public class checkoutGUI{
+
+    JFrame f = new JFrame("Checkout GUI");
+
+    Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+    int height = screenSize.height;
+    int width = screenSize.width;
+    int width_item = 0;
+    int item_width = (int) (width * 0.092);
+
+    public checkoutGUI(){
+        JButton cashButton = new JButton("CASH");
+        cashButton.setBounds(130,100,100, 40);
+        
+                ////////// Menu-items Area //////////
+        JPanel itemsPanel = new JPanel();
+        itemsPanel.setBackground(Color.yellow);
+        itemsPanel.setBounds((int) (width * 0.06), (int) (height * 0.09), (int) (width * 0.6), (int) (height * 0.7));
+        itemsPanel.setLayout(new GridLayout(5, 2, 10, 10)); 
+
+        itemsPanel.add(cashButton);
+
+        f.add(itemsPanel);
+
+        f.setSize(screenSize.width, screenSize.height);
+        f.setLayout(null);
+        f.setVisible(true);
+    }
+    public static void main(String args[]) {
+        new checkoutGUI();
+    }
+}
